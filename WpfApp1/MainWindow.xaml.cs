fbfa18fd43f001e923f1cf6dc09acad6a01a1d9d
predatorerd <predatorerd@gmail.com>
2025-04-03 20:39:46 +0200

    Corregido changelog

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace WpfApp1
 {
     public partial class MainWindow : Window
     {
-        string Version = "0.4.1";
+        string Version = "0.5.0";
 
         List<DatosClientesWrapper> itemsDatosClientes = new List<DatosClientesWrapper>();
         private GridViewColumnHeader listViewSortCol = null;
